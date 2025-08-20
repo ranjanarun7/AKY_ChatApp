4d5a36a12c1da862af534b66e547d5b57d7fcce3
ranjanarun7 <ranjanarunbabu@gmail.com>
2025-08-20 09:49:55 +0530

    #

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT,()=>{
     dbConnect();
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider=({children})=>{
     const {authUser} = useAuth();
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5173",{
+            const socket = io("https://aky-chatapp.onrender.com" || "http://localhost:5173",{
                 query:{
                     userId:authUser?._id,
                 }
