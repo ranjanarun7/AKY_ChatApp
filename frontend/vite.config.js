ab0b29b43ee4fe22abd23d0011dbf555f7b70e0b
ranjanarun7 <ranjanarunbabu@gmail.com>
2025-08-20 09:03:39 +0530

    Added deploy link

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:5000" || "https://aky-chatapp.onrender.com",
         changeOrigin: true,
       },
     },
